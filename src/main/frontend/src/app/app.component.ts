e2138ed1133e90755e55678fb7cfc8c06bc18351
Mikael Gueck <gumi@iki.fi>
2016-11-12 21:06:05 +0200

    Simple state fetch.

--- src/main/frontend/src/app/app.component.ts
+++ src/main/frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Http } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  private title = 'Hello, World!';
+
+  private state = {
+    name: 'Initial',
+    counter: -1,
+    timestamp: ''
+  };
+
+  constructor(private http: Http) { }
+
+  public ngOnInit(): void {
+    Observable.interval(1000)
+      .flatMap(() => this.http.get('/api/hello'))
+      .forEach(response => this.state = response.json());
+  }
+
 }
--- src/main/kotlin/io/mikael/app/app.kt
+++ src/main/kotlin/io/mikael/app/app.kt
@@ -2,9 +2,27 @@ package io.mikael.app
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.OffsetDateTime
+import java.util.concurrent.atomic.AtomicLong
 
 @SpringBootApplication
 open class Application {
+
+}
+
+@RestController
+open class AppController {
+
+    private val counter = AtomicLong()
+
+    @GetMapping("/api/hello")
+    fun hello() = mapOf(
+            "name" to "World",
+            "counter" to counter.getAndIncrement().toString(),
+            "timestamp" to OffsetDateTime.now().toString()
+    )
 
 }
 
