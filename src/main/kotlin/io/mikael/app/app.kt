b629577f0d90bb3ec09b2b3c39a84ad7523be789
Mikael Gueck <gumi@iki.fi>
2017-02-16 00:48:08 +0200

    Fix build.

--- src/main/kotlin/io/mikael/app/app.kt
+++ src/main/kotlin/io/mikael/app/app.kt
@@ -2,18 +2,29 @@ package io.mikael.app
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.OffsetDateTime
 import java.util.concurrent.atomic.AtomicLong
 
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
+
 @SpringBootApplication
-open class Application {
+open class Application
+
+@Controller
+open class SinglePageController {
+
+    @GetMapping("/", "/app", "/app/", "/app/**")
+    fun singlePageApplication() = "forward:index.html"
 
 }
 
 @RestController
-open class AppController {
+open class ApiController {
 
     private val counter = AtomicLong()
 
@@ -24,8 +35,4 @@ open class AppController {
             "timestamp" to OffsetDateTime.now().toString()
     )
 
-}
-
-fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
 }
